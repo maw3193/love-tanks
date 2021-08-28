5df65ba09d68951215b89309cd628464dbb7a20f
Jonathan Maw <jonathan.maw@gmail.com>
2021-08-28 20:40:36 +0100

    Fix readability of generated MoveOrders
    Restricts generated MoveOrder positions to integer coordinates

--- lib/wander-order.lua
+++ lib/wander-order.lua
@@ -15,8 +15,8 @@ function WanderOrder:update(dt, isFirstOrder)
         local radius = math.random(0, self.wanderRange)
         local angle = math.random() * math.pi * 2
         local px, py = self.executor.body:getPosition()
-        px = px + radius * math.cos(angle)
-        py = py + radius * math.sin(angle)
+        px = math.floor(px + radius * math.cos(angle))
+        py = math.floor(py + radius * math.sin(angle))
         local moveTarget = Waypoint(self.executor.game, {x=px, y=py})
         local moveOrder = MoveOrder{target=moveTarget}
         self.executor:prependOrder(moveOrder)
